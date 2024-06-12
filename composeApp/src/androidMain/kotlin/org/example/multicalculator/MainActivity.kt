388cfbb93712bae97e1cc0c7c585c3cdbccb4bce
va00297223 <vvirani@mycambrian.ca>
2024-06-12 16:19:02 -0400

    CalcView Function Added

--- composeApp/src/androidMain/kotlin/org/example/multicalculator/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/multicalculator/MainActivity.kt
@@ -4,8 +4,24 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,4 +37,35 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AppAndroidPreview() {
     App()
+}
+
+@Composable
+fun CalcView() {
+    val displayText = remember { mutableStateOf("0") }
+
+    Column(modifier = Modifier
+        .background(Color.LightGray)
+        .fillMaxWidth()
+        .height(350.dp)){
+        Row {
+            CalcDisplay(display = displayText)
+        }
+        Row {
+            Column {
+                for (i in 7 downTo 1 step 3) {
+                    CalcRow(display = displayText, startNum = i, numButtons = 3)
+                }
+                Row {
+                    CalcNumericButton(number = 0, display = displayText)
+                    CalcEqualsButton(display = displayText)
+                }
+            }
+            Column {
+                val operations = listOf("+", "-", "*", "/")
+                for (operation in operations) {
+                    CalcOperationButton(operation = operation, display = displayText)
+                }
+            }
+        }
+    }
 }
